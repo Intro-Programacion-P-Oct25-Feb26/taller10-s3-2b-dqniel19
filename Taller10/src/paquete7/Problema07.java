0daf832dda8aeddcf60fee1321b9b23d83e9bdeb
Daniel <danielp4p1@gmail.com>
2025-12-18 12:43:59 -0500

    Resolucion parcial del taller

--- Taller10/src/paquete7/Problema07.java
+++ Taller10/src/paquete7/Problema07.java
@@ -4,10 +4,35 @@
  */
 package paquete7;
 
+import java.util.Scanner;
+
 /**
  *
  * @author UTPL
  */
 public class Problema07 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
     
+        double[][] matriz = new double[3][3];
+        int[][] A = new int[3][3];
+        int[][] B = new int[3][3];
+        
+        System.out.println("\nIngrese los valores de la matriz (3x3):");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.printf("Elemento [%d][%d]: ", i, j);
+                matriz[i][j] = entrada.nextDouble();    
+            }   
+        }
+        
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+               /* int x = matriz[i][j];
+                A[i][j] = (x + 1) * (x + 1);
+                B[i][j] = x * x + 2 * x + 1; */
+            }
+        }
+        // s 
+}
 }
