c40106de73b205aec6f11790fb6dd0aa766fcdc0
Daniel <danielp4p1@gmail.com>
2025-12-18 11:20:25 -0500

    commit

--- Taller10/src/paquete2/Problema02.java
+++ Taller10/src/paquete2/Problema02.java
@@ -10,5 +10,35 @@ package paquete2;
  * @author reroes
  */
 public class Problema02 {
-    
+
+    public static void main(String[] args) {
+        int[][] edades = {
+            {5, 12, 35, 40},
+            {8, 15, 33, 38},
+            {6, 10, 30, 36}
+        };
+
+        int mayores[] = new int[3];
+        int menores = 0;
+        String msj = "";
+
+        for (int i = 0; i < edades.length; i++) {
+            for (int g = 0; g < edades[i].length; g++) {
+                if (edades[i][g] < 18) {
+                menores = menores + 1;
+                } else {
+                    mayores[i]++;
+                }
+            }  
+        }
+        
+        for (int n = 0; n < 3; n++) { // no se usó .length debido a que se debia imprimir "familia 1"
+            msj = String.format("%sMayores de edad en la familia %d: %d\n", 
+                    msj, n + 1, mayores[n]);
+        }
+        System.out.printf("Reporte de edades\n---------------------------\n"
+                + "Total de menores de edad en todas las familias: %d\n"
+                + "%s",
+                menores, msj);
+    }
 }
